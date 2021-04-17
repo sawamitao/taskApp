6cdf6367efbefc3f8a60fe368923aedc37e0447e
Sawami Tao <macaron.3031@icloud.com>
2021-04-17 11:19:47 +0900

    taskApp初めてのコミット

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '14.2'
+
+target 'taskApp' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+  source 'https://github.com/CocoaPods/Specs.git'
+
+  # Pods for taskApp
+  pod 'RealmSwift','10.1.4' 
+
+end
--- taskApp/ViewController.swift
+++ taskApp/ViewController.swift
@@ -7,13 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
 
+
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view.
     }
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "cellSegue",sender: nil)
+    }
+    // セルが削除が可能なことを伝えるメソッド
+    func tableView(_ tableview:UITableView,editingStyleForRowAt indexPath: IndexPath)-> UITableViewCell.EditingStyle{
+        return .delete
+    }
+    // Delete ボタンが押された時に呼ばれるメソッド
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+    }
 
 }
 

a25af0c3cb2c87018d68fa64e3626cb725ea742a
Sawami Tao <macaron.3031@icloud.com>
2021-04-17 11:39:24 +0900

    通知の実装を追加

--- taskApp/AppDelegate.swift
+++ taskApp/AppDelegate.swift
@@ -6,16 +6,26 @@
 //
 
 import UIKit
-
+import UserNotifications
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate{
 
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let center = UNUserNotificationCenter.current()
+                center.requestAuthorization(options: [.alert, .sound]) { (granted, error) in
+                    // Enable or disable features based on authorization
+                }
+                center.delegate = self
+        
+        
         // Override point for customization after application launch.
         return true
     }
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+            completionHandler([.banner, .list, .sound])
+        }
 
     // MARK: UISceneSession Lifecycle
 
--- taskApp/InputViewController.swift
+++ taskApp/InputViewController.swift
@@ -6,19 +6,40 @@
 //
 
 import UIKit
-
+import RealmSwift
 class InputViewController: UIViewController {
 
     @IBOutlet weak var titleTextField: UITextField!
     
     @IBOutlet weak var contentsTextView: UITextView!
     @IBOutlet weak var datePicker: UIDatePicker!
-    
+    let realm = try! Realm()
+    var task: Task!
     override func viewDidLoad() {
         super.viewDidLoad()
+        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target:self, action:#selector(dismissKeyboard))
+                self.view.addGestureRecognizer(tapGesture)
+
+                titleTextField.text = task.title
+                contentsTextView.text = task.contents
+                datePicker.date = task.date
 
         // Do any additional setup after loading the view.
     }
+    override func viewWillDisappear(_ animated: Bool) {
+            try! realm.write {
+                self.task.title = self.titleTextField.text!
+                self.task.contents = self.contentsTextView.text
+                self.task.date = self.datePicker.date
+                self.realm.add(self.task, update: .modified)
+            }
+
+            super.viewWillDisappear(animated)
+        }
+    @objc func dismissKeyboard(){
+            // キーボードを閉じる
+            view.endEditing(true)
+        }
     
 
     /*
